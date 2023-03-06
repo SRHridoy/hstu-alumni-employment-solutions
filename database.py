bc5f1e0ddf2147ac1a0161a339d1df615fe0e5c7
Md. Sohanur Rahman Hridoy <115940778+SRHridoy@users.noreply.github.com>
2023-03-06 16:40:15 +0000

    create application form and submission form and add them to database...Finally Finished my work...

--- database.py
+++ database.py
@@ -32,3 +32,25 @@ def load_job_from_db(id):
 
 
 
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+        conn.execute(
+            query.bindparams(
+                job_id=job_id,
+                full_name=data['full_name'],
+                email=data['email'],
+                linkedin_url=data['linkedin_url'],
+                education=data['education'],
+                work_experience=data['work_experience'],
+                resume_url=data['resume_url']
+            )
+        )
+
+
+
+
+
+
+  
