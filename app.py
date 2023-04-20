86d875efdddf892c01c70dabdf9667e3a875e4de
SRHridoy <115940778+SRHridoy@users.noreply.github.com>
2023-04-20 12:49:34 +0000

    ....

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db,load_job_from_db,add_application_to_db
 
 app = Flask(__name__)
-
+#123
 @app.route("/")
 def hello_HAES():
   jobs_list = load_jobs_from_db()
