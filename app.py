5d9de41489b196ab72aa50586d5089142a66e3c4
SRHridoy <115940778+SRHridoy@users.noreply.github.com>
2023-04-23 16:21:28 +0000

    from tomorrow

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db,load_job_from_db,add_application_to_db
-
+#কাল থেকে ইনশাআল্লাহ 
 app = Flask(__name__)
 #123
 @app.route("/")
